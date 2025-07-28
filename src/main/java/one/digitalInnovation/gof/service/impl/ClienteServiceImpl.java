55e3bf766166d623a8ea403d4378afa74ded28b0
Renato dos Santos <rnt.dsantos@gmail.com>
2025-07-28 20:47:36 -0300

    Efetuado os TODO

--- src/main/java/one/digitalInnovation/gof/service/impl/ClienteServiceImpl.java
+++ src/main/java/one/digitalInnovation/gof/service/impl/ClienteServiceImpl.java
@@ -21,32 +21,54 @@ import one.digitalinnovation.gof.service.ViaCepService;
  */
 @Service
 public class ClienteServiceImpl implements ClienteService {
-
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private ViaCepService viaCepService;
 	
 
 	@Override
 	public Iterable<Cliente> buscarTodos() {
+		return clienteRepository.findAll();
 
 	}
 
 	@Override
 	public Cliente buscarPorId(Long id) {
-	;
+	Optional<Cliente> cliente = clienteRepository.finfById(id);
+	return cliente.get();
 	}
 
 	@Override
 	public void inserir(Cliente cliente) {
-		
+		salvarClienteComCep(cliente);		
 	}
 
 	@Override
 	public void atualizar(Long id, Cliente cliente) {
+		Optional<Cliente> clienteBd = clienteRepository.finfById(id);
+		if(clienteBd.isPresent()){
+			salvarClienteComCep(cliente);
+		}
 		
+	}
+
+	private void salvarClienteComCep(Cliente cliente){
+		String cep = cliente.getEndereco().getCep();
+		Endereco endereco = enderecoRepository.finfById(cep).orElseGet(() -> {
+		Endereco novoEndereco = viaCepService.consultarCep(cep);
+		enderecoRepository.save(novoEndereco);	
+		return novoEndereco;
+		});
+		cliente.setEndereco(endereco);
+		clienteRepository.save(cliente);
 	}
 
 	@Override
 	public void deletar(Long id) {
-	
+		clienteRepository.deletarById(id);
 	}
 
 	
